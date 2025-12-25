20b2c4c45ddc37cc6865c582dc7c0ed135d4dbf1
Alex <Rieznik.O.A@nmu.one>
2025-12-25 02:13:48 +0200

    Solution#2

--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ class Dictionary:
         self.capacity = len(self._cells)
         self._threshold = 2 / 3 * self.capacity
         self.length = 0
+        self._tombstone = object()
 
     def resize(self) -> None:
         new_cells = [None] * (self.capacity * 2)
         new_cells_threshold = 2 / 3 * len(new_cells)
 
         for cell in self._cells:
-            if cell is not None:
+            if cell is not None or not self._tombstone:
                 new_id = cell[1] % len(new_cells)
                 while new_cells[new_id] is not None:
                     new_id = (new_id + 1) % len(new_cells)
@@ -45,7 +46,7 @@ class Dictionary:
         cell_index = hash(key) % len(self._cells)
         while True:
             if self._cells[cell_index] is None:
-                raise KeyError
+                raise KeyError(f"Key {key} not found")
             elif self._cells[cell_index][0] == key:
                 return self._cells[cell_index][2]
             else:
@@ -58,22 +59,25 @@ class Dictionary:
         cell_index = hash(key) % len(self._cells)
         while True:
             if self._cells[cell_index][0] == key:
-                self._cells[cell_index] = None
+                self._cells[cell_index] = self._tombstone
                 self.length -= 1
                 break
             else:
                 cell_index = (cell_index + 1) % len(self._cells)
 
     def keys(self) -> Any:
-        return (cell[0] for cell in self._cells if cell is not None)
+        return (cell[0] for cell in self._cells
+                if cell is not None or not self._tombstone)
 
     def values(self) -> Any:
-        return (cell[2] for cell in self._cells if cell is not None)
+        return (cell[2] for cell in self._cells
+                if cell is not None or not self._tombstone)
 
     def items(self) -> Any:
-        return ((cell[0], cell[2]) for cell in self._cells if cell is not None)
+        return ((cell[0], cell[2]) for cell in self._cells
+                if cell is not None or not self._tombstone)
 
-    def update(self, dictionary: dict | "Dictionary") -> None:
+    def update(self, dictionary: dict) -> None:
 
         for key, value in dictionary.items():
             self.__setitem__(key, value)
